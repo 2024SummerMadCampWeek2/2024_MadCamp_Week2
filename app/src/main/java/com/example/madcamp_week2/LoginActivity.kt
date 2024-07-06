da1f04018c6b0eda9baeffb94c768fb20821dc9f
gnwsb <2646back@kaist.ac.kr>
2024-07-06 21:18:40 +0900

    UI 수정

--- app/src/main/java/com/example/madcamp_week2/LoginActivity.kt
+++ app/src/main/java/com/example/madcamp_week2/LoginActivity.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.madcamp_week2.databinding.ActivityLoginBinding
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
 import com.kakao.sdk.user.UserApiClient
 
 class LoginActivity : AppCompatActivity() {
@@ -20,13 +23,34 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun loginWithKakao() {
-        UserApiClient.instance.loginWithKakaoAccount(this) { token, error ->
+        UserApiClient.instance.loginWithKakaoTalk(this) { token, error ->
             if (error != null) {
-                // Handle login error
+                if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+                    // 사용자가 카카오톡 로그인 취소
+                } else if (error is ClientError && error.reason == ClientErrorCause.Unknown) {
+                    // 카카오톡이 설치되지 않은 경우
+                    loginWithKakaoAccount()
+                } else {
+                    // 기타 오류 처리
+                }
             } else if (token != null) {
-                startActivity(Intent(this, MainActivity::class.java))
-                finish()
+                startMainActivity()
             }
         }
+    }
+
+    private fun loginWithKakaoAccount() {
+        UserApiClient.instance.loginWithKakaoAccount(this) { token, error ->
+            if (error != null) {
+                // 로그인 오류 처리
+            } else if (token != null) {
+                startMainActivity()
+            }
+        }
+    }
+
+    private fun startMainActivity() {
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
     }
 }
--- app/src/main/java/com/example/madcamp_week2/NaverAPI.kt
+++ app/src/main/java/com/example/madcamp_week2/NaverAPI.kt
@@ -53,12 +53,9 @@ data class BookSearchResponse(
 
 data class Book(
     val title: String,
-    val link: String,
     val image: String,
     val author: String,
-    val discount: String,
     val publisher: String,
     val pubdate: String,
     val isbn: String,
-    val description: String
 )
