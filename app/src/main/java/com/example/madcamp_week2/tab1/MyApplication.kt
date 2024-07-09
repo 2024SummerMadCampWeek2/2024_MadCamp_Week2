e414d607282c3fd56c7aa03dedf1f3702f485461
gnwsb <2646back@kaist.ac.kr>
2024-07-10 02:12:09 +0900

    카카오 키해쉬

--- app/src/main/java/com/example/madcamp_week2/tab1/MyApplication.kt
+++ app/src/main/java/com/example/madcamp_week2/tab1/MyApplication.kt
@@ -1,12 +1,18 @@
 package com.example.madcamp_week2.tab1
 
 import android.app.Application
+import android.util.Log
 import com.kakao.sdk.common.KakaoSdk
+import com.kakao.sdk.common.util.Utility
 
 class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         // Initialize Kakao SDK
         KakaoSdk.init(this, "9e167b564736611eeda3fac0e447d065")
+
+        // Get and log the key hash
+        val keyHash = Utility.getKeyHash(this)
+        Log.d("MyApplication", "KeyHash: $keyHash")
     }
 }
